4aa0d6233f0b248c782b1d9501f07231feae92e2
IGabriel <gabriel_cgq@hotmail.com>
2018-02-03 23:07:26 +0800

    add file reader

---
+++ Common/FileReader.cs
@@ -0,0 +1,11 @@
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class FileReader
+    {
+        const string testFolder = @"C:\TestData\destination";
+        
+    }
+}
