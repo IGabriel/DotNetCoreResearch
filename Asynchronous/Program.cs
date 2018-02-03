98b5ed6267976ab50450cf2a13db64e077691c63
IGabriel <gabriel_cgq@hotmail.com>
2018-02-03 23:47:54 +0800

    Async IO, test #1

--- Asynchronous/Program.cs
+++ Asynchronous/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Common;
 
 namespace Asynchronous
 {
@@ -7,6 +9,22 @@ namespace Asynchronous
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            const string sourceFolder = @"C:\TestData\source";
+            const string destinationFolder = @"C:\TestData\destination";
+            const string filter = @"*.txt";
+
+            ClearDestinationFolder(destinationFolder);
+            FileReader reader = new FileReader();
+            reader.ReadCopyFolderAsync(sourceFolder, destinationFolder, filter);
+        }
+
+        private static void ClearDestinationFolder(string folder)
+        {
+            foreach(var fileName in Directory.EnumerateFiles(folder))
+            {
+                File.Delete(fileName);
+            }
         }
     }
 }
--- Common/FileReader.cs
+++ Common/FileReader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -5,7 +6,21 @@ namespace Common
 {
     public class FileReader
     {
-        const string testFolder = @"C:\TestData\destination";
-        
+        public async Task ReadCopyFolderAsync(string sourceFolder, string destinationFolder, string filter)
+        {
+            foreach(string fileName in Directory.EnumerateFiles(sourceFolder, filter))
+            {
+                string destinationFile = Path.Combine(destinationFolder, Path.GetFileName(fileName));
+                using(var sourceStream = File.Open(fileName, FileMode.Open))
+                {
+                    using(var destinationStream = File.Create(destinationFile))
+                    {
+                        Console.WriteLine("Copying file, from '{0}' to '{1}'", fileName, destinationFile);
+                        await sourceStream.CopyToAsync(destinationStream);
+                        Console.WriteLine("Copied file, from '{0}' to '{1}'", fileName, destinationFile);
+                    }
+                }
+            }
+        }
     }
 }
